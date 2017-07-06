7ec9d74bfc92e023ae9392643cf74c9f34fd522a
wooheaven <wooheaven79@gmail.com>
2017-07-07 02:42:59 +0900

    #30-2017-07-05-chrome_match-html-firefox_player_skill_table-txt 수정함:        01_KP_table.md 수정함:        02_WB_table.md 수정함:        03_CD_table.md 수정함:        04_W_table.md 수정함:        05_IM_table.md 수정함:        06_FW_table.md 새 파일:       20170705/chrome_match.html 새 파일:       20170705/chrome_match.txt 새 파일:       20170705/firefox_player_skill_table.txt 수정함:        htmlToObject.ipynb 새 파일:       parsing_chrome_match.sh

---
+++ 00_Data/00_Player/parsing_chrome_match.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+unset targetFile
+unset tmpFile
+unset isReady
+
+if [[ -e $1 ]];
+then 
+echo "Ready to parsing chrome_match.txt";
+
+targetFile="$1/chrome_match.txt"
+tmpFile="$1/tmpFile.txt"
+isReady="true"
+
+else 
+echo "Not ready to parsing chrome_match.txt";
+fi
+
+if [[ $isReady -eq "true" ]];
+then
+
+sed -i 's/,{/\n{/g' $targetFile
+awk 'BEGIN{FS=OFS=","} {print $2,$1,$4,$10,$11}' $targetFile > $tmpFile
+mv $tmpFile $targetFile
+
+# Change PlayerID to Number
+awk 'BEGIN{FS=OFS=","} {if( substr($2,13) == "427619232" ) print "{"$1,"99",$3,$4,$5; else }' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( substr($2,13) == "427619233" ) print "{"$1,"1",$3,$4,$5; else }' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( substr($2,13) == "427619238" ) print "{"$1,"6",$3,$4,$5; else }' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( substr($2,13) == "427619235" ) print "{"$1,"3",$3,$4,$5; else }' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( substr($2,13) == "427619239" ) print "{"$1,"7",$3,$4,$5; else }' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( substr($2,13) == "427619240" ) print "{"$1,"8",$3,$4,$5; else }' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( substr($2,13) == "427619245" ) print "{"$1,"13",$3,$4,$5; else }' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( substr($2,13) == "427619244" ) print "{"$1,"12",$3,$4,$5; else }' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( substr($2,13) == "427619249" ) print "{"$1,"17",$3,$4,$5; else }' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( substr($2,13) == "427619241" ) print "{"$1,"9",$3,$4,$5; else }' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( substr($2,13) == "425734621" ) print "{"$1,"26",$3,$4,$5; else }' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( substr($2,13) == "428125893" ) print "{"$1,"23",$3,$4,$5; else }' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( substr($2,13) == "427619242" ) print "{"$1,"10",$3,$4,$5; else }' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( substr($2,13) == "427619248" ) print "{"$1,"16",$3,$4,$5; else }' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( substr($2,13) == "427619250" ) print "{"$1,"18",$3,$4,$5; else }' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( substr($2,13) == "428709461" ) print "{"$1,"2",$3,$4,$5; else }' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( substr($2,13) == "425173438" ) print "{"$1,"24",$3,$4,$5; else }' $targetFile >> $tmpFile
+mv $tmpFile $targetFile
+
+# PositionID to Pos
+sed -i 's/}//' $targetFile
+
+awk -v oldPos="0" -v oldFromMin="-1" 'BEGIN{FS=OFS=","} {if( substr($1,15) == "-1" && substr($5,9) == oldFromMin ) print oldPos,$2,$3,$4,$5; else print substr($1,15),$2,$3,$4,$5; oldPos=substr($1,15); oldFromMin=substr($4,11)}' $targetFile >> $tmpFile
+mv $tmpFile $targetFile
+
+tac $targetFile > $tmpFile
+mv $tmpFile $targetFile
+
+awk -v oldPos="0" -v oldToMin="-1" 'BEGIN{FS=OFS=","} {if( $1 == "-1" && substr($4,11) == oldToMin ) print oldPos,$2,$3,$4,$5; else print $1,$2,$3,$4,$5; oldPos=$1; oldToMin=substr($5,9)}' $targetFile >> $tmpFile
+mv $tmpFile $targetFile
+
+tac $targetFile > $tmpFile
+mv $tmpFile $targetFile
+
+sort -t, -k1n,1 -k4,4 $targetFile > $tmpFile
+mv $tmpFile $targetFile
+
+awk 'BEGIN{FS=OFS=","} {if( $1 == "0" ) print "CO",$2,$3,$4,$5; else}' $targetFile >> $tmpFile
+echo "" >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( $1 == "100" ) print "KP",$2,$3,$4,$5; else}' $targetFile >> $tmpFile
+echo "" >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( $1 == "101" ) print "WB",$2,$3,$4,$5; else}' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( $1 == "105" ) print "WB",$2,$3,$4,$5; else}' $targetFile >> $tmpFile
+echo "" >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( $1 == "102" ) print "CD",$2,$3,$4,$5; else}' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( $1 == "104" ) print "CD",$2,$3,$4,$5; else}' $targetFile >> $tmpFile
+echo "" >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( $1 == "106" ) print "W",$2,$3,$4,$5; else}' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( $1 == "110" ) print "W",$2,$3,$4,$5; else}' $targetFile >> $tmpFile
+echo "" >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( $1 == "107" ) print "IM",$2,$3,$4,$5; else}' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( $1 == "109" ) print "IM",$2,$3,$4,$5; else}' $targetFile >> $tmpFile
+echo "" >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( $1 == "111" ) print "FW",$2,$3,$4,$5; else}' $targetFile >> $tmpFile
+awk 'BEGIN{FS=OFS=","} {if( $1 == "113" ) print "FW",$2,$3,$4,$5; else}' $targetFile >> $tmpFile
+mv $tmpFile $targetFile
+
+echo ""
+cat $targetFile
+
+fi
+
+unset targetFile
+unset tmpFile
+unset isReady
