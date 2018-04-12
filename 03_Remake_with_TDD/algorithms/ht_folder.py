1397cdf25e82f64afbaab0c0ba20c575d661d932
rwoo <wooheaven79@gmail.com>
2018-04-12 22:24:59 +0900

    #221-2018-04-12-lk ``` modified:   ../00_LastKeeper/00_memo_Player.md modified:   ../00_LastKeeper/01_KP_table.md modified:   ../00_LastKeeper/02_WB_table.md modified:   ../00_LastKeeper/03_CD_table.md modified:   ../00_LastKeeper/04_W_table.md modified:   ../00_LastKeeper/05_IM_table.md modified:   ../00_LastKeeper/06_FW_table.md new file:   ../00_LastKeeper/2018/04/11/match.html new file:   ../00_LastKeeper/2018/04/11/match.txt new file:   ../00_LastKeeper/2018/04/11/player.txt modified:   ../00_LastKeeper/parsing_match.ipynb modified:   ../00_LastKeeper/parsing_player.ipynb new file:   algorithms/ht_file.py deleted:    algorithms/ht_folder.py deleted:    test/test_folder.py new file:   test/test_ht_file.py renamed:    test/test_hattrickTime.py -> test/test_ht_time.py ```

--- 03_Remake_with_TDD/algorithms/ht_folder.py
+++
@@ -1,9 +0,0 @@
-import os.path
-
-
-class HattrickFolder():
-    def check_and_create_folder(self, folder):
-        while (not os.path.exists(folder)):
-            print(folder + "\t\t : will be created")
-            os.makedirs(folder)
-        print(folder + '\t\t : is existed')
