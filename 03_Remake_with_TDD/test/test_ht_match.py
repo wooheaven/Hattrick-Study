a692bea7f3843bfbf054291c4c3cf1055773a575
rwoo <wooheaven79@gmail.com>
2018-04-26 00:02:55 +0900

    #228-2018-04-25-lk ``` modified:   00_LastKeeper/00_memo_Player.md modified:   00_LastKeeper/01_KP_table.md modified:   00_LastKeeper/02_WB_table.md modified:   00_LastKeeper/03_CD_table.md modified:   00_LastKeeper/04_W_table.md modified:   00_LastKeeper/05_IM_table.md modified:   00_LastKeeper/06_FW_table.md modified:   00_LastKeeper/07_Stadium.md new file:   00_LastKeeper/2018/04/25/match.html new file:   00_LastKeeper/2018/04/25/match.txt new file:   00_LastKeeper/2018/04/25/player.txt modified:   00_LastKeeper/parsing_match.ipynb modified:   00_LastKeeper/parsing_player.ipynb new file:   03_Remake_with_TDD/algorithms/ht_match.py new file:   03_Remake_with_TDD/test/2018-04-25/match.html new file:   03_Remake_with_TDD/test/test_ht_match.py ```

---
+++ 03_Remake_with_TDD/test/test_ht_match.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+import unittest
+from algorithms import ht_match
+
+class TestHattrickMatch(TestCase):
+    """
+    Test Home or Away matchList
+    """
+    def test_findMatchList(self):
+        instance = ht_match.HattrickMatch()
+        result = instance.findMatchList('2018-04-25/match.html')
+        print(result)
