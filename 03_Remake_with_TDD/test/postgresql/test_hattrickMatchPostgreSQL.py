e333f8cfccf56f93e4276573dd33a39473f419fd
rwoo <wooheaven79@gmail.com>
2018-05-26 17:27:13 +0900

    #255-update-match-po-sql
    ```
    modified:   00_LastKeeper/00_memo_Player.md
    modified:   00_LastKeeper/parsing_match.ipynb
    modified:   01_Heavy2Defence/00_memo_Player.md
    modified:   01_Heavy2Defence/parsing_match.ipynb
    new file:   03_Remake_with_TDD/postgresql/ht_match_postgresql.py
    new file:   03_Remake_with_TDD/test/postgresql/__init__.py
    new file:   03_Remake_with_TDD/test/postgresql/test_hattrickMatchPostgreSQL.py
    modified:   README.md
    ```

---
+++ 03_Remake_with_TDD/test/postgresql/test_hattrickMatchPostgreSQL.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+import psycopg2
+from postgresql import ht_match_postgresql
+
+class TestHattrickMatchPostgreSQL(TestCase):
+    def test_getMatchByPoNum(self):
+        conn = psycopg2.connect("dbname='mydatabase' user='myuser' host='localhost' port='65432' password='123qwe'")
+        hattrickMatchPostgresql = ht_match_postgresql.HattrickMatchPostgreSQL()
+        tupleList = hattrickMatchPostgresql.getMatchByPoNum(conn, 'FW', [16, 18, 31, 48, 55])
+        hattrickMatchPostgresql.printMatchByPoNum(tupleList, [16, 18, 31, 48, 55])
