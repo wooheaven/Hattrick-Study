298c9c0a6745182dea0e736477e3f3c78422d1a4
rwoo <wooheaven79@gmail.com>
2018-06-18 01:31:24 +0900

    #267-2018-06-17-lk-hd
    ```
    modified:   00_LastKeeper/00_memo_Player.md
    modified:   00_LastKeeper/2018/06/13/match.txt
    new file:   00_LastKeeper/2018/06/17/match.html
    new file:   00_LastKeeper/2018/06/17/match.txt
    new file:   00_LastKeeper/2018/06/17/player.txt
    modified:   00_LastKeeper/parsing_match.ipynb
    modified:   00_LastKeeper/parsing_player.ipynb
    new file:   01_Heavy2Defence/2018/06/17/match.html
    new file:   01_Heavy2Defence/2018/06/17/match.txt
    new file:   01_Heavy2Defence/2018/06/17/player.txt
    modified:   01_Heavy2Defence/parsing_match.ipynb
    modified:   01_Heavy2Defence/parsing_player.ipynb
    modified:   03_Remake_with_TDD/postgresql/ht_match_postgresql.py
    modified:   03_Remake_with_TDD/test/postgresql/test_hattrickMatchPostgreSQL.py
    modified:   README.md
    ```

--- 03_Remake_with_TDD/postgresql/ht_match_postgresql.py
+++ 03_Remake_with_TDD/postgresql/ht_match_postgresql.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+
 class HattrickMatchPostgreSQL():
     def getMatchByPoNum(self, conn, po, numList):
         tupleList = list();
@@ -59,6 +60,39 @@ class HattrickMatchPostgreSQL():
             print(error)
         return tupleList
 
+    def getMatchByDate(self, date, conn):
+        tuple_list = []
+        query = ""
+        try:
+            cur = conn.cursor()
+            query += "SELECT                                    " + "\n"
+            query += "    po, num, rt :: VARCHAR(5), smin, emin " + "\n"
+            query += "FROM                                      " + "\n"
+            query += "    match                                 " + "\n"
+            query += "WHERE                                     " + "\n"
+            query += "    date ='" + date.replace('/', '-') + "'" + "\n"
+            query += "ORDER BY                                  " + "\n"
+            query += "    CASE                                  " + "\n"
+            query += "        WHEN po = 'KP' THEN 1             " + "\n"
+            query += "        WHEN po = 'WB' THEN 2             " + "\n"
+            query += "        WHEN po = 'CD' THEN 3             " + "\n"
+            query += "        WHEN po = 'W'  THEN 4             " + "\n"
+            query += "        WHEN po = 'IM' THEN 5             " + "\n"
+            query += "        WHEN po = 'FW' THEN 6             " + "\n"
+            query += "        ELSE 7                            " + "\n"
+            query += "    END,                                  " + "\n"
+            query += "    emin asc, smin desc                   " + "\n"
+            cur.execute(query)
+            row = cur.fetchone()
+            while row is not None:
+                tuple_list.append(row)
+                row = cur.fetchone()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(query)
+            print("Error Happend")
+            print(error)
+        return tuple_list
+
     def printMatchByPoNum(self, tupleList, numList):
         headStr = "date num min/rt |"
         for num in numList:
@@ -84,3 +118,10 @@ class HattrickMatchPostgreSQL():
             return "         |"
         else:
             return " " + str(column) + " |"
+
+    def printMatchByDate(self, tuple_list):
+        for tuple in tuple_list:
+            line_str = str(tuple[0])
+            for index in range(1, len(tuple)):
+                line_str += "\t" + str(tuple[index])
+            print(line_str)
--- 03_Remake_with_TDD/test/postgresql/test_hattrickMatchPostgreSQL.py
+++ 03_Remake_with_TDD/test/postgresql/test_hattrickMatchPostgreSQL.py
@@ -2,9 +2,15 @@ from unittest import TestCase
 import psycopg2
 from postgresql import ht_match_postgresql
 
+
 class TestHattrickMatchPostgreSQL(TestCase):
     def test_getMatchByPoNum(self):
         conn = psycopg2.connect("dbname='mydatabase' user='myuser' host='localhost' port='65432' password='123qwe'")
         hattrickMatchPostgresql = ht_match_postgresql.HattrickMatchPostgreSQL()
         tupleList = hattrickMatchPostgresql.getMatchByPoNum(conn, 'FW', [16, 18, 31, 48, 55])
         hattrickMatchPostgresql.printMatchByPoNum(tupleList, [16, 18, 31, 48, 55])
+    def test_getMatchByDate(self):
+        conn = psycopg2.connect("dbname='mydatabase' user='myuser' host='localhost' port='65432' password='123qwe'")
+        hattrickMatchPostgresql = ht_match_postgresql.HattrickMatchPostgreSQL()
+        tupleList = hattrickMatchPostgresql.getMatchByDate("2018/06/17", conn)
+        hattrickMatchPostgresql.printMatchByDate(tupleList)
