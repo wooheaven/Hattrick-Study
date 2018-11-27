38f312ca20005ad680e7bf32dd913797107b7ecd
rwoo <wooheaven79@gmail.com>
2018-11-27 23:14:20 +0900

    #346-2018-11-25-lk-hd
    ```
    new file:   00_LastKeeper/2018/11/25/match.html
    new file:   00_LastKeeper/2018/11/25/match.txt
    new file:   00_LastKeeper/2018/11/25/player.csv
    modified:   00_LastKeeper/parsing_match.ipynb
    modified:   00_LastKeeper/parsing_player.ipynb
    modified:   00_LastKeeper/screen-Copy.png
    new file:   01_Heavy2Defence/2018/11/25/match.html
    new file:   01_Heavy2Defence/2018/11/25/match.txt
    new file:   01_Heavy2Defence/2018/11/25/player.csv
    modified:   01_Heavy2Defence/parsing_match.ipynb
    modified:   01_Heavy2Defence/parsing_player.ipynb
    modified:   01_Heavy2Defence/screen-Copy.png
    modified:   98_Crawler/04_Login/HattrickLogInOut.py
    ```

--- 98_Crawler/04_Login/HattrickLogInOut.py
+++ 98_Crawler/04_Login/HattrickLogInOut.py
@@ -84,36 +84,6 @@ def clickHattrickLogin(x, y, isDualMonitor):
 	# wait
 	time.sleep(6)
 
-def clickFacebookLogout(x, y, isDualMonitor):
-	# find locateOnScreen
-	if isDualMonitor:
-		x += 1920
-
-	# move
-	pyautogui.moveTo(x, y, duration=0.15)
-
-	# find faceBookLogin
-	faceBookLogout = pyautogui.locateOnScreen('screen-FacebookLogout.png', region=(x, y, 69, 51), grayscale=True)
-	if None == faceBookLogout:
-		faceBookLogout = pyautogui.locateOnScreen('screen-FacebookLogout.png')
-		if None == faceBookLogout:
-			print('1st 2nd faceBookLogout is None')
-		else:
-			print('2nd faceBookLogout location =', faceBookLogout)
-			x = faceBookLogout[0] + faceBookLogout[2]/2
-			y = faceBookLogout[1] + faceBookLogout[3]/2
-	else:
-		print('1st faceBookLogout location with region =', faceBookLogout)
-		x = faceBookLogout[0] + faceBookLogout[2]/2
-		y = faceBookLogout[1] + faceBookLogout[3]/2
-
-	# move and click
-	pyautogui.moveTo(x, y, duration=0.2)
-	pyautogui.click()
-
-	# wait
-	time.sleep(3)
-
 def clickHattrickLogout(x, y, isDualMonitor):
 	# define position
 	if isDualMonitor:
