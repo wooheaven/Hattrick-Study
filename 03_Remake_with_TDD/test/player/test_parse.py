b4b19dd87f11a72279416b56d3d9ee617bdddffc
rwoo <wooheaven79@gmail.com>
2019-04-02 02:07:48 +0900

    #379-2019-03-31-wed-lk-hd
    ```
    modified:   00_LastKeeper/00_memo_Player.md
    modified:   00_LastKeeper/01_parsing_player.ipynb
    new file:   00_LastKeeper/2019/03/31/match.html
    new file:   00_LastKeeper/2019/03/31/player.csv
    modified:   00_LastKeeper/screen-Copy.png
    modified:   00_LastKeeper/screen-Since.png
    modified:   00_LastKeeper/screen-SkillTable.png
    modified:   01_Heavy2Defence/00_memo_Player.md
    modified:   01_Heavy2Defence/01_parsing_player.ipynb
    new file:   01_Heavy2Defence/2019/03/31/match.html
    new file:   01_Heavy2Defence/2019/03/31/match.txt
    new file:   01_Heavy2Defence/2019/03/31/player.csv
    modified:   01_Heavy2Defence/screen-Copy.png
    modified:   01_Heavy2Defence/screen-Since.png
    modified:   01_Heavy2Defence/screen-SkillTable.png
    modified:   03_Remake_with_TDD/algorithms/ht_match.py
    modified:   03_Remake_with_TDD/player/parse.py
    modified:   03_Remake_with_TDD/player/parse_tmp.py
    new file:   03_Remake_with_TDD/test/2019/03/31/match.html
    new file:   03_Remake_with_TDD/test/2019/03/31/player.csv
    new file:   03_Remake_with_TDD/test/2019/03/31/player.html
    new file:   03_Remake_with_TDD/test/2019/03/31/player_tmp.html
    modified:   03_Remake_with_TDD/test/player/test_parse.py
    deleted:    03_Remake_with_TDD/test/player/test_parse_tmp.py
    modified:   03_Remake_with_TDD/test/test_ht_match.py
    ```

--- 03_Remake_with_TDD/test/player/test_parse.py
+++ 03_Remake_with_TDD/test/player/test_parse.py
@@ -3,7 +3,7 @@ from player import parse
 
 
 class TestParse(TestCase):
-    def test_parse_player_html_to_csv(self):
+    def test_parse_player_html_to_csv_2018_10_07(self):
         p = parse.Parse()
         p.read_line_list('../2018/10/07/player.html')
         p.print_line_list()
@@ -12,3 +12,23 @@ class TestParse(TestCase):
         p.print_df_cols()
         p.print_df()
         p.save_df('../2018/10/07/player.csv')
+
+    def test_parse_player_html_to_csv_2018_11_07(self):
+        p = parse.Parse()
+        p.read_line_list('../2018/11/07/player.html')
+        p.print_line_list()
+        p.write_line_list('../2018/11/07/player_tmp.html')
+        p.modify_line_list('../2018/11/07/player_tmp.html')
+        p.print_df_cols()
+        p.print_df()
+        p.save_df('../2018/11/07/player.csv')
+
+    def test_parse_player_html_to_csv_2019_03_31(self):
+        p = parse.Parse()
+        p.read_line_list('../2019/03/31/player.html')
+        p.print_line_list()
+        p.write_line_list('../2019/03/31/player_tmp.html')
+        p.modify_line_list('../2019/03/31/player_tmp.html')
+        p.print_df_cols()
+        p.print_df()
+        p.save_df('../2019/03/31/player.csv')
